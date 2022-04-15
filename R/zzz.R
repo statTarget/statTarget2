e5a8c65287462d6498bda2a88ada471020cfd8ca
13479776 <hemi.luan@gmail.com>
2022-04-15 17:59:53 +0800

    delete

--- R/zzz.R
+++
@@ -1,4 +0,0 @@
-.onAttach <- function(...) {
-    packageStartupMessage("\nUse 'statTargetGUI()' to start the GUI program. For details see https://stattarget.github.io.\n")
-    # statTarget::statTargetGUI()
-}
